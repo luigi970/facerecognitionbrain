7265159206bd2fda85f42010a0de02f0ce938ccf
way2net <way2net@iMac-de-way2net.local>
2021-03-11 10:58:43 -0300

    corrección del logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import brain from './brain.png';
 const Logo = () => {
 	return (
 		<div className="ma4 mt0">
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner">
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+				<div className="Tilt-inner pa4">
 					<img style={{paddingTop: '5px'}} alt="Logo" src={brain} />
 				</div>
 			</Tilt>
