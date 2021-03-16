ae672bc3bbc8bea06273d659b20e3e124aceefa7
luis <luis_devesa@hotmail.com>
2021-03-16 06:54:13 -0300

    Conexion a la api de clarifai y primeros datos recibidos de reconocimiento facial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -17,19 +20,55 @@ const particlesOptions = {
 	}
 }
 
-function App() {
-  return (
-    <div className="App">
-    	<Particles className="particles" 
-    	 params={particlesOptions}
-    	/>
-		<Navigation />
-		<Logo />
-		<Rank />
-		<ImageLinkForm />
-        {/*    <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+			imageUrl: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value})
+		console.log(event.target.value);
+	}
+
+	onButtonSubmit = () => {
+		this.setState({imageUrl: this.state.input});
+
+		let app = new Clarifai.App({apiKey: '22a65b0736674a048b69dd5bc55543da'});
+
+		app.models
+			.predict({
+				id:'d02b4508df58432fbb84e800597b8959'
+			}, this.state.input)
+			.then(
+		  function(response) {
+		    // do something with response
+		    console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+		  },
+		  function(err) {
+		    // there was an error
+		  }
+		);
+		console.log('click');
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles" 
+				 params={particlesOptions}
+				/>
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+			    <FaceRecognition imageUrl={this.state.imageUrl} />
+			</div>
+		);
+	}
 }
 
 export default App;
